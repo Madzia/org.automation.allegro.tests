179ef0203db7eb5976fdcc88a03fdf7088316758
Madzia <magda2609@gmail.com>
2015-04-08 23:04:36 +0200

    Implementation for: opening web page, enter 'smartwatch', click on the magnifier button & search results sorted by 'cena: od najwyższej'.

---
+++ README.md
@@ -0,0 +1,34 @@
+Magdalena Sarzyńska - Automation Tests
+
+##Lauch
+
+In case of project lauch you need to :
+
+### clone my repository and build/run project
+
+* By enter in shell:
+```js
+git clone https://github.com/Madzia/org.automation.allegro.tests.git
+cd org.automation.allegro.tests
+```
+* Load project into Eclipse IDE.
+* Run AutomationTest.
+
+##Test elements:
+
+1. Open webpage 'www.allegro.pl'.
+2. Enter 'smartwatch' into search field.
+3. Click the magnifier button.
+4. search results sorted by 'cena: od najwyższej'.
+5. Click at first auction in list.
+6. In item website verify that price is bigger than 1000 zł.
+7. Open 'Dostawa i płatność'.
+8. Download and write into log available supply options.
+
+##Requirements
+
+* Eclipse IDE (or another one, preferred version is Luna Service Release 2 (4.4.2) or newer). 
+* Java SE 7 (or later).
+* Maven (with Eclipse Marketplace synchronisation).
+* TestNG (with Eclipse Marketplace synchronisation).
+* Selenium (will be downloaded by Maven).
--- src/test/java/org/kainos/automation/allegro/tests/AutomationTests.java
+++ src/test/java/org/kainos/automation/allegro/tests/AutomationTests.java
@@ -1,5 +1,57 @@
 package org.kainos.automation.allegro.tests;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class AutomationTests {
+
+	private WebDriver driver;
+	private String websiteUrl = new String("http://www.allegro.pl");
+	private String searchedPhrase = new String("smartwatch");
+
+	@BeforeClass(alwaysRun = true)
+	public void setUp() {
+		driver = new FirefoxDriver();
+		driver.manage()
+			.timeouts()
+			.implicitlyWait(5, TimeUnit.SECONDS);
+	}
 	
+	@BeforeMethod(alwaysRun = true)
+	public void openHomePage() {
+		driver.get(websiteUrl);
+	}
+	
+	@AfterClass(alwaysRun = true)
+	public void tearDown() {
+		driver.quit();
+	}
+	
+	@Test
+	public void testSupplyOptionsForMostExpensiveSmartwatch() {
+		//Enter into text field searched phrase
+		driver.findElement(By.id("main-search-text")).sendKeys(searchedPhrase);
+		
+		//Click the magnifier button
+		driver.findElement(By.xpath("//input[@class='sprite search-btn']")).click();
+		
+		//Locate the web elements
+		WebElement sortPicklist = driver.findElement(By.xpath("//div[@class='listing-sort-dropdown']/descendant::span[@class='arrow']"));
+        WebElement sortHighest = driver.findElement(By.xpath("//dd[2]/child::a[1]"));
+		
+        //Select "cena: od najwyższej"
+		new Actions(driver).click(sortPicklist)
+		.click(sortHighest)
+		.perform();
+		
+	}
 }
